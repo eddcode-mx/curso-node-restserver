5a803c998c4f8ac837721aac324ec45b37292ad2
Diego Rmz <eddcodemx@gmail.com>
2021-06-09 03:46:07 -0500

    cambio del puerto a 8080

--- models/server.js
+++ models/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 class Server {
     constructor() {
         this.app = express();
-        this.port = 3000;
+        this.port = 8080;
         this.usuariosPath = '/api/usuarios';
 
         // Middlewares
